0e37b6fd38b756b8d70e254e9d251c5d475f0a35
langliu <langliu1216@gmail.com>
2025-03-02 00:04:01 +0800

    feat: ✨ 更新依赖

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,4 @@
-import { dirname, join } from 'node:path'
 import type { StorybookConfig } from 'storybook-react-rsbuild'
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
-}
 
 const config: StorybookConfig = {
   stories: [
@@ -21,14 +12,9 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    {
-      name: getAbsolutePath('storybook-addon-rslib'),
-    },
+    'storybook-addon-rslib',
   ],
-  framework: {
-    name: getAbsolutePath('storybook-react-rsbuild'),
-    options: {},
-  },
+  framework: 'storybook-react-rsbuild',
   docs: {
     autodocs: 'tag',
   },
